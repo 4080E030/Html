7f8362b2ea1c01b4b672ef17fa5370560df45b2e
bradchen <55682616+4080E030@users.noreply.github.com>
2021-06-03 15:53:34 +0800

    Create 自學html.py

---
+++ 自學html.py
@@ -0,0 +1,22 @@
+```html 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>熊貓問卷調查</title>
+</head>
+<body>
+    <form  action=""  method="">
+        <label for="myname">姓名:</label>
+        <input id="myname" type="text" name="InputName"/>
+        <label for="tele">電話</label>
+        <input id="tele"   type="text" name="telephone ">
+
+
+        <button type="submit">提交表單</button>
+    </form>
+</body>
+</html>
+```
